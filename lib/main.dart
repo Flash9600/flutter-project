12e9a7e50905a6db37b453df881b648f67f5a810
flash9600 <trefilovdimarus@gmail.com>
2021-07-11 23:02:31 +0400

    feat: button with url_launcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: scaffold
-  )
-);
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: scaffold));
 }
 
-var scaffold = new Scaffold(
-    appBar: new AppBar(title: new Text('Flutter.su')),
-    body: new Center(
-      child: new Column(
-        children: [new Text('Hello world'), new TextButton(onPressed: (){}, child: new Text('open site'))],
-      ),
-    )
-);
+var scaffold =
+    Scaffold(appBar: AppBar(title: Text('Flutter.su')), body: MyBody());
 
-class MyBody extends StatelessWidget{
+class MyBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Center(
+      child: Column(
+        children: [
+          Text('Hello world'),
+          ElevatedButton(
+              onPressed: () async {
+                print('object');
+                const url = 'https://flutter.dev';
+                if (await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  var error = await canLaunch(url);
+                  print(error);
+                }
+              },
+              style: ElevatedButton.styleFrom(primary: Colors.red[800]),
+              child: Text('open new world',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400)))
+        ],
+      ),
+    );
   }
 }
