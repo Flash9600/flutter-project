2372cec8fe839d89c45971415284d12c7f511dd1
flash9600 <trefilovdimarus@gmail.com>
2021-07-12 12:17:50 +0400

    feat: add drawer, bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,52 @@ void main() {
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: scaffold));
 }
 
-var scaffold =
-    Scaffold(appBar: AppBar(title: Text('Flutter.su')), body: MyBody());
+var scaffold = Scaffold(
+    appBar: AppBar(title: Text('Flutter.su')),
+    body: MyBody(),
+    floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blue,
+        splashColor: Colors.red,
+        child: Icon(Icons.add)),
+    drawer: Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(color: Colors.blue),
+            child: Text('My first Drawer',
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600)),
+          ),
+          ListTile(
+            onTap: () {},
+            title: Text('item 1'),
+            hoverColor: Colors.blue,
+          )
+        ],
+      ),
+    ),
+    floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    bottomNavigationBar: BottomAppBar(
+        color: Colors.blue,
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10,
+        child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: IconTheme(
+                data: IconThemeData(color: Colors.white, size: 32),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+                        IconButton(onPressed: () {}, icon: Icon(Icons.phone)),
+                      ],
+                    )
+                  ],
+                )))));
 
 class MyBody extends StatelessWidget {
   @override
