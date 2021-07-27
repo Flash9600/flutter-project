9d756ad92f1cb77cb110a703d822842da469cbe4
Dmitrii Trefilov1 <Dmitrii_Trefilov1@epam.com>
2021-07-27 10:24:08 +0400

    implement InheritedModel with aspects

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,12 @@ class Example extends StatelessWidget {
   }
 }
 
+class IValue {
+  static const String value = 'value';
+  static const String valueTwo = 'valueTwo';
+  IValue();
+}
+
 class OwnerStatefull extends StatefulWidget {
   const OwnerStatefull({Key? key}) : super(key: key);
 
@@ -24,10 +30,16 @@ class OwnerStatefull extends StatefulWidget {
 }
 
 class _OwnerStatefullState extends State<OwnerStatefull> {
-  var _value = 54;
+  var _value = 0;
+  var _valueTwo = 0;
 
   void _incriment() {
     _value += 1;
+    setState(() {});
+  }
+
+  void _incrimentTwo() {
+    _valueTwo += 1;
     setState(() {});
   }
 
@@ -39,22 +51,42 @@ class _OwnerStatefullState extends State<OwnerStatefull> {
           onPressed: _incriment,
           child: const Text('Tap'),
         ),
-        ProviderInherited(value: _value, child: const TextStateless()),
+        ElevatedButton(
+          onPressed: _incrimentTwo,
+          child: const Text('Tap two'),
+        ),
+        ProviderInherited(
+            value: _value, valueTwo: _valueTwo, child: const TextStateless()),
       ],
     );
   }
 }
 
-class ProviderInherited extends InheritedWidget {
+class ProviderInherited extends InheritedModel<String> {
   final int value;
+  final int valueTwo;
 
   const ProviderInherited(
-      {Key? key, required this.value, required Widget child})
+      {Key? key,
+      required this.value,
+      required this.valueTwo,
+      required Widget child})
       : super(key: key, child: child);
 
   @override
   bool updateShouldNotify(ProviderInherited oldWidget) {
-    return value != oldWidget.value;
+    return value != oldWidget.value || valueTwo != oldWidget.valueTwo;
+  }
+
+  @override
+  bool updateShouldNotifyDependent(
+      ProviderInherited old, Set<String> dependencies) {
+    if (dependencies.contains(IValue.value)) {
+      return value != old.value;
+    } else if (dependencies.contains(IValue.valueTwo)) {
+      return valueTwo != old.valueTwo;
+    }
+    return false;
   }
 }
 
@@ -64,11 +96,36 @@ class TextStateless extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final value = context
-            .dependOnInheritedWidgetOfExactType<ProviderInherited>()
+            .dependOnInheritedWidgetOfExactType<ProviderInherited>(
+                aspect: IValue.value)
             ?.value ??
         0;
-    return Container(
-      child: Text('$value'),
-    );
+    return Column(children: [
+      Text('$value'),
+      ChildStatefull(),
+    ]);
+  }
+}
+
+class ChildStatefull extends StatefulWidget {
+  const ChildStatefull({Key? key}) : super(key: key);
+
+  @override
+  _ChildStatefullState createState() => _ChildStatefullState();
+}
+
+class _ChildStatefullState extends State<ChildStatefull> {
+  var value;
+
+  didChangeDependencies() {
+    value = context
+        .dependOnInheritedWidgetOfExactType<ProviderInherited>(
+            aspect: IValue.valueTwo)
+        ?.valueTwo;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('$value');
   }
 }
